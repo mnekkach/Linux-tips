0112abae182fe0582afea3963e332adf36b156fc
mnekkach <mnekkach@gmail.com>
2025-08-03 17:18:36 +0200

    Update the tips binary with new options --help --list

--- bin/tips.bash
+++ bin/tips.bash
@@ -1,28 +1,62 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-VERSION=1.0.0
+VERSION="1.0.0"
 TIPS_SHARE_DIR="$HOME/.local/share/linux-tips"
 
-if [ "$1" = "--version" ]
-then
-    echo "Version $VERSION"
-    exit 0
-fi
-if [ -z "$1" ]
-    then
-	echo "No argument supplied"
-else
-    FILE="$TIPS_SHARE_DIR/$1-tips"
-    if [[ -f "$FILE" ]]
-	then
-            cat "$FILE"
+show_help() {
+    cat <<EOF
+Linux Tips - Command line tips
+
+Usage:
+  tips [topic]        Show tips for the given topic
+  tips --list         List all available tips
+  tips --version      Show the current version
+  tips --help         Show this help message
+EOF
+}
+
+show_list() {
+    if [ -d "$TIPS_SHARE_DIR" ]; then
+        echo "-----------------------------"
+        echo "Available tips:"
+        echo "-----------------------------"
+        ls -1 "$TIPS_SHARE_DIR" | nl | sed 's/^\s*\([0-9]\+\)\s\(.*\)$/\1| \2/'
     else
-	    echo "ERROR :: Tips for $1 does not exist !"
-	    echo "-----------------------------"
-	    echo "List of the available tips :"
-	    echo "-----------------------------"
-	    ls -1 $TIPS_SHARE_DIR | nl | sed 's/^\s*\([0-9]\+\)\s\(.*\)$/\1| \2/'
+        echo "ERROR :: Tips directory not found at $TIPS_SHARE_DIR"
     fi
-    
+}
+
+# Handle no arguments
+if [ $# -eq 0 ]; then
+    echo "No argument supplied. Use --help for usage."
+    exit 1
 fi
 
+# Handle options
+case "$1" in
+    --version)
+        echo "Linux Tips version $VERSION"
+        exit 0
+        ;;
+    --help)
+        show_help
+        exit 0
+        ;;
+    --list)
+        show_list
+        exit 0
+        ;;
+esac
+
+# Handle tips for a specific topic
+TOPIC="$1"
+FILE="$TIPS_SHARE_DIR/$TOPIC-tips"
+
+if [ -f "$FILE" ]; then
+    cat "$FILE"
+else
+    echo "ERROR :: Tips for '$TOPIC' do not exist!"
+    echo
+    show_list
+    exit 1
+fi
